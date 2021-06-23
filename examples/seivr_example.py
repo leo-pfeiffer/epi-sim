92dd8a854615d95c07377f846e037d09bb65d92e
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-06-23 10:39:54 +0100

    Added convenience class ExperimentRunner

--- examples/seivr_example.py
+++ examples/seivr_example.py
@@ -1,12 +1,10 @@
 from epydemic import ERNetwork, StochasticDynamics, Monitor
-import matplotlib.pyplot as plt
-from epyc import Experiment
 
 from model.compartmental_model.seivr import MonitoredSEIVR, SEIVR
+from experiments.runner import ExperimentRunner
 
 params = dict()
 
-T = 1000
 # use an ER network as the substrate
 params[ERNetwork.N] = N = 10000
 params[ERNetwork.KMEAN] = k_mean = 3
@@ -21,38 +19,20 @@ params[SEIVR.P_VACCINATED_INITIAL] = 0.0
 params[SEIVR.P_VACCINATED] = 0.0002
 params[SEIVR.VACCINE_RRR] = 0.95
 
-# capture every 10 timesteps
+# capture every 10 time steps
 params[Monitor.DELTA] = 10
 
 # run the compound process
-seivr = MonitoredSEIVR()
-e = StochasticDynamics(seivr, g=ERNetwork())
-e.process().setMaximumTime(T)
-rc = e.set(params).run()
+e = StochasticDynamics(MonitoredSEIVR(), g=ERNetwork())
 
-res = rc[Experiment.RESULTS]
+compartments = {
+    SEIVR.SUSCEPTIBLE: 'susceptible',
+    SEIVR.EXPOSED: 'exposed',
+    SEIVR.INFECTED: 'infected',
+    SEIVR.VACCINATED: 'vaccinated',
+    SEIVR.REMOVED: 'removed'
+}
 
-if not rc[Experiment.METADATA][Experiment.STATUS]:
-    raise BaseException('You fool.')
-
-ts = res[Monitor.OBSERVATIONS]
-er_sss = [v / N for v in res[Monitor.timeSeriesForLocus(SEIVR.SUSCEPTIBLE)]]
-er_ees = [v / N for v in res[Monitor.timeSeriesForLocus(SEIVR.EXPOSED)]]
-er_iis = [v / N for v in res[Monitor.timeSeriesForLocus(SEIVR.INFECTED)]]
-er_vvs = [v / N for v in res[Monitor.timeSeriesForLocus(SEIVR.VACCINATED)]]
-er_rrs = [v / N for v in res[Monitor.timeSeriesForLocus(SEIVR.REMOVED)]]
-
-fig = plt.figure(figsize=(5, 5))
-ax = fig.gca()
-plt.title(f'Progress over an ER network \n $(N = {N})$')
-plt.xlabel('time')
-ax.set_xlim([0, T])
-plt.ylabel('fraction of population that is...')
-ax.set_ylim([0.0, 1.0])
-plt.plot(ts, er_sss, 'y', label='susceptible')
-plt.plot(ts, er_ees, 'r', label='exposed')
-plt.plot(ts, er_iis, 'b', label='infected')
-plt.plot(ts, er_vvs, 'm', label='vaccinated')
-plt.plot(ts, er_rrs, 'g', label='removed')
-plt.legend(loc='upper right')
-plt.show()
+er = ExperimentRunner(e, N)
+er.run(params, T=1000)
+er.plot('My title', compartments)
---
+++ experiments/runner.py
@@ -0,0 +1,103 @@
+from typing import Any, Dict, Union, List, Tuple, Iterable
+from epyc import ResultsDict, Experiment
+from epydemic import Dynamics, Monitor
+
+from matplotlib import pyplot as plt
+
+
+class ExperimentRunner:
+    """
+    Wrapper around the epydemic `Dynamics` (sub-)classes to streamline some
+    of the code required for running a single experiment and plotting the
+    result.
+    """
+
+    def __init__(self, dynamics: Dynamics, N: int):
+        """
+        Constructor.
+        :param dynamics: Dynamics of the experiment.
+        :param N: Size of the network... for convenience since this way we don't
+            have to awkwardly try to access the network.
+        """
+        self._dynamics = dynamics
+        self._N = N
+        self._result: Union[ResultsDict, None] = None
+
+    @property
+    def dynamics(self):
+        return self._dynamics
+
+    @property
+    def result(self):
+        return self._result
+
+    def run(self, params: Dict[str, Any], T: int = 1000, fatal: bool = True):
+        """
+        Run the experiment
+        :param params: Experiment parameters
+        :param T: Maximum time of the experiment
+        :param fatal: If True, exceptions in the experiment are raised. Else
+            they are stored in the results dict.
+        """
+
+        self._dynamics.process().setMaximumTime(T)
+        self._dynamics.set(params)
+        rc = self._dynamics.run(fatal=fatal)
+        self._result = rc[Experiment.RESULTS]
+
+    def get_time_series(self, compartments: Iterable[str]) -> \
+            Tuple[List[float], Dict[str, List[float]]]:
+        """
+        Get the time series of the population in the compartments specified.
+        :param compartments: Any iterable of compartment keys.
+        :return: List of time steps, List of time series lists
+        """
+
+        times = self.result[Monitor.OBSERVATIONS]
+        series = dict()
+
+        for c in compartments:
+            series_abs = self.result[Monitor.timeSeriesForLocus(c)]
+            series_rel = [v / self._N for v in series_abs]
+            series[c] = series_rel
+
+        return times, series
+
+    @staticmethod
+    def _color_string():
+        """
+        Simple color string generator. Not that if it is called more than
+        `len(colors)` times, the colours are not unique.
+        :return: Generator object
+        """
+        colors = ['r', 'y', 'g', 'c', 'b', 'm', 'peru', 'pink', 'gray']
+        i = 0
+        while True:
+            i %= len(colors)
+            yield colors[i]
+            i += 1
+
+    def plot(self, title: str, compartments: Dict[str, str]):
+        """
+        Plot the results of the experiment.
+        :param title: Title of the plot
+        :param compartments: Compartment keys with labels.
+        """
+
+        times, series = self.get_time_series(compartments.keys())
+        T = self._dynamics.process().maximumTime()
+        color_gen = self._color_string()
+
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.gca()
+        plt.title(title)
+        plt.xlabel('time')
+        ax.set_xlim([0, T])
+        ax.set_ylim([0.0, 1.0])
+        plt.ylabel('fraction of population')
+
+        for c in compartments:
+            plt.plot(times, series[c], next(color_gen), label=compartments[c])
+
+        plt.legend(loc='upper right')
+        plt.show()
