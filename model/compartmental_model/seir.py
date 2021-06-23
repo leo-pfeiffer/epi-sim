e8f12fd17fcacf804ee8f226436dfc85f40e2029
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-06-23 16:54:10 +0100

    Added SEIRWithQuarantine

--- model/compartmental_model/seir.py
+++ model/compartmental_model/seir.py
@@ -1,4 +1,20 @@
-from epydemic import SEIR, Monitor
+from typing import Final
+
+from epydemic import SEIR, Monitor, Edge
+from model.compartmental_model.mixins import QuarantineMixin
+
+
+class SEIRWithQuarantine(SEIR, QuarantineMixin):
+
+    P_QUARANTINE: Final[str] = 'epydemic.SEIRWithQuarantine.p_quarantine'  #: Parameter for probability of removal (recovery).
+
+    def __init__(self):
+        super(SEIRWithQuarantine, self).__init__()
+
+    def infect(self, t : float, e : Edge):
+        super(SEIRWithQuarantine).infect(t, e)
+        n, _ = e
+        self.quarantine(n)
 
 
 class MonitoredSEIR(SEIR, Monitor):
