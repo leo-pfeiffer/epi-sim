Fixed faulty variance and mean in PLC

model/distributions.py
@@ -66,7 +66,10 @@ class PowerLawCutoffDist:
         Mean of the distribution.
         :return: mean.
         """
-        return polylog(self._tau-1, np.exp(-1 / self._kappa))
+        # todo use epydemic gf
+        n = polylog(self._tau - 1, np.exp(-1 / self._kappa))
+        m = polylog(self._tau, np.exp(-1 / self._kappa))
+        return n / m
 
     @property
     def var(self) -> mpf:
@@ -74,7 +77,13 @@ class PowerLawCutoffDist:
         Variance of the distribution.
         :return: variance.
         """
-        return polylog(self._tau-2, np.exp(-1 / self._kappa))
+        # todo use epydemic gf
+        x = np.exp(-1 / self._kappa)
+        n = polylog(self._tau - 2, x) - polylog(self._tau - 1, x)
+        m = polylog(self._tau, x)
+        g = n / m
+
+        return g + self.mean - self.mean * self.mean
 
 
 # todo make names a bit more generic

tests/test_distributions.py
@@ -144,11 +144,20 @@ def test_plc_mean():
     """
     Test PLC distribution returns correct mean.
     """
-    assert PLC_DIST.mean == polylog(TAU-1, np.exp(-1/KAPPA))
+    n = polylog(TAU-1, np.exp(-1 / KAPPA))
+    m = polylog(TAU, np.exp(-1 / KAPPA))
+
+    assert PLC_DIST.mean == n / m
 
 
 def test_plc_variance():
     """
     Test PLC distribution returns correct variance.
     """
-    assert PLC_DIST.var == polylog(TAU-2, np.exp(-1/KAPPA))
+    x = np.exp(-1 / KAPPA)
+    n = polylog(TAU-1, x)
+    m = polylog(TAU, x)
+
+    g = (polylog(TAU-2, x) - polylog(TAU-1, x)) / polylog(TAU, x)
+
+    assert PLC_DIST.var == g + (n/m) - (n/m) * (n/m)