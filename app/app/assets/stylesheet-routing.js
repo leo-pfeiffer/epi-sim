16e1f0743f608238200ad154b70691ee1f2853e4
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-13 13:59:15 +0100

    Fixed buggy style sheet routing

--- app/app/assets/stylesheet-routing.js
+++ app/app/assets/stylesheet-routing.js
@@ -1,3 +1,13 @@
+const addStyleSheet = function(url) {
+    const link = document.createElement('link')
+    link.href = url
+    link.rel = 'stylesheet'
+
+    const head = document.querySelector('head')
+    head.appendChild(link)
+}
+
+
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         switchStylesheet: function(url) {
@@ -8,19 +18,31 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             const INDEX_CSS = '/assets/index-page.css'
             const TEXT_CSS = '/assets/text-page.css'
 
+            const INDEX_NODE = document.querySelector(INDEX_SELECTOR)
+            const TEXT_NODE = document.querySelector(TEXT_SELECTOR)
 
+
+            // text pages
             if (url !== '/') {
-                const stylesheet = document.querySelector(INDEX_SELECTOR)
-
-                if (stylesheet !== null) {
-                    stylesheet.href = TEXT_CSS
+                // add text page CSS if it doesn't exist
+                if (TEXT_NODE === null) {
+                    addStyleSheet(TEXT_CSS)
+                }
+                // remove index page CSS if it exists
+                if (INDEX_NODE !== null) {
+                    INDEX_NODE.remove()
                 }
             }
-            else {
-                const stylesheet = document.querySelector(TEXT_SELECTOR)
 
-                if (stylesheet !== null) {
-                    stylesheet.href = INDEX_CSS
+            // index page
+            else {
+                // add index page CSS if it doesn't exist
+                if (INDEX_NODE === null) {
+                    addStyleSheet(INDEX_CSS)
+                }
+                // remove text page CSS if it exists
+                if (TEXT_NODE !== null) {
+                    TEXT_NODE.remove()
                 }
             }
         }
