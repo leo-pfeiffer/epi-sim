b10d60f706cb4db2b25e7d251d4f0e348e34625b
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-13 18:41:27 +0100

    Added text from markdown

--- app/app/pages/data.py
+++ app/app/pages/data.py
@@ -1,12 +1,18 @@
 import dash_html_components as html
-
+import dash_core_components as dcc
 from ..static_elements import brand, footer
+import os
 
-some_text = html.Div(
-    html.Div(html.P('This is the data page'), id='text-content'),
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(path, 'markdown', 'data.md'), 'r') as f:
+    markdown = f.read()
+
+text = html.Div(
+    html.Div(dcc.Markdown(markdown), id='text-content'),
     id='text-container'
 )
 
 data_page = [
-    brand, footer, some_text
+    brand, footer, text
 ]
--- app/app/pages/model.py
+++ app/app/pages/model.py
@@ -1,12 +1,17 @@
 import dash_html_components as html
-
+import dash_core_components as dcc
 from ..static_elements import brand, footer
+import os
 
-some_text = html.Div(
-    html.Div(html.P('This is the model page'), id='text-content'),
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(path, 'markdown', 'model.md'), 'r') as f:
+    markdown = f.read()
+
+text = html.Div(
+    html.Div(dcc.Markdown(markdown), id='text-content'),
     id='text-container'
 )
-
 model_page = [
-    brand, footer, some_text
+    brand, footer, text
 ]
