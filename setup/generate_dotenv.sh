81ffb4a341725418e15410449a085f9742d8550e
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-18 14:57:14 +0100

    Fixed script

--- setup/generate_dotenv.sh
+++ setup/generate_dotenv.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -euo pipefail
+set -eu pipefail
 
 # make sure this is run from project root
 expected_path=./setup/generate_dotenv.sh
 actual_path=$0
 
-if [[ "$expected_path" != "$actual_path" ]]; then
+if [ "$expected_path" != "$actual_path" ]; then
   echo "Please run me from the project root" >&2
   exit 1
 fi
 
 # check if ./lib/.env exists
-if [[ ! -f ./lib/.env ]] ; then
+if [ ! -f ./lib/.env ] ; then
   echo "  >> [add] ./lib/.env"
   cat ./setup/github.env > ./lib/.env
 else
@@ -19,7 +19,7 @@ else
 fi
 
 # check if ./app/.env exists
-if [[ ! -f ./app/.env ]] ; then
+if [ ! -f ./app/.env ] ; then
   echo "  >> [add] ./app/.env"
   cat ./setup/docker.env > ./app/.env
   echo >> ./app/.env
