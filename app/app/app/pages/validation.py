081b1eb77bfa26b0d08ba47fc0c1a87ece26249d
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-28 15:33:32 +0100

    Get data from repo

--- app/app/app/pages/validation.py
+++ app/app/app/pages/validation.py
@@ -8,6 +8,7 @@ from plotly.graph_objects import Figure
 import pandas as pd
 import pickle
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import logging
 
 from ..mixins import SimulationTransformerMixin
@@ -27,9 +28,9 @@ class ValidationData(ABC):
         url = DATA_REPO_URL_RAW + '/validation/' + file + '.pkl'
 
         with urlopen(url) as f:
-            df = pickle.load(f)
+            obj = pickle.load(f)
 
-        return df
+        return obj
 
     @staticmethod
     def transform_covid(df):
@@ -81,17 +82,28 @@ class EmpiricalData(ValidationData):
     US_FILE = 'validation/case-data/us_covid_count_by_state.csv'
     POP_FILE = 'validation/case-data/us_population_by_state.csv'
 
+    ALL_STATES_FILE_NAME = 'app-data/all_states'
+
     def __init__(self):
         super().__init__()
-        self.covid_us = self.get_csv_from_repo(self.US_FILE)
-        self.pop_map = self.make_state_population()
-        self.first_wave_map = self.make_first_wave_map()
-        self.all_states = self.make_all_states()
+        self._covid_us = self.get_csv_from_repo(self.US_FILE)
+        self._pop_map = self.make_state_population()
+        self._first_wave_map = self.make_first_wave_map()
+
+        try:
+            self._states = self.get_pickle_from_repo(self.ALL_STATES_FILE_NAME)
+            logging.info('Loaded states data from repo')
+        except HTTPError:
+            self._states = self.make_all_states()
+
+    @property
+    def states(self):
+        return self._states
 
     @classmethod
     def make_state_population(cls):
 
-        logging.root.info('Calculating state population')
+        logging.info('Calculating state population')
 
         df = cls.get_csv_from_repo(cls.POP_FILE)
 
@@ -111,15 +123,15 @@ class EmpiricalData(ValidationData):
 
         logging.info('Compiling first wave map')
 
-        df = self.transform_covid(self.covid_us)
+        df = self.transform_covid(self._covid_us)
 
         first_wave_map = {}
 
         for state in df.state.unique():
             subset = df[df.state == state].sort_values('date')
-            if state not in self.pop_map:
+            if state not in self._pop_map:
                 continue
-            threshold_cases = self.pop_map[state] * threshold
+            threshold_cases = self._pop_map[state] * threshold
             date = subset[subset.tot_cases >= threshold_cases].iloc[0].date
             first_wave_map[state] = date.strftime("%Y-%m-%d")
 
@@ -135,16 +147,16 @@ class EmpiricalData(ValidationData):
 
     def make_state_covid(self, state):
 
-        start_date = self.first_wave_map[state]
+        start_date = self._first_wave_map[state]
 
-        df = self.transform_covid(self.covid_us)
+        df = self.transform_covid(self._covid_us)
 
         state_filter = {'column': 'state', 'value': state}
         df_state = self.extract_region(df, state_filter)
 
         df_state = self.get_data_after_date(df_state, start_date)
 
-        pop = self.pop_map[state]
+        pop = self._pop_map[state]
         df_state = self.make_counts_relative(df_state, pop)
 
         return df_state
@@ -155,7 +167,7 @@ class EmpiricalData(ValidationData):
 
         dfs = []
 
-        for state in self.pop_map:
+        for state in self._pop_map:
             if state == 'NYC':
                 continue
             else:
@@ -177,10 +189,21 @@ class ModelledData(ValidationData, SimulationTransformerMixin):
         {'name': 'v_seirq_75_mobility_post', 'title': 'SEIR_Q (p=0.75), M (Post)'},
     ]
 
+    RESULTS_FILE_NAME = 'app-data/results'
+
     def __init__(self):
         super().__init__()
-        self.results = []
+        self._results = []
         self.make_validation_data()
+        try:
+            self._results = self.get_pickle_from_repo(self.RESULTS_FILE_NAME)
+            logging.info('Loaded results data from repo')
+        except HTTPError:
+            self.make_validation_data()
+
+    @property
+    def results(self):
+        return self._results
 
     def get_result(self, name):
         for r in self.results:
@@ -199,7 +222,7 @@ class ModelledData(ValidationData, SimulationTransformerMixin):
         for v in self.VALIDATION_FILES:
             r = v.copy()
             r['data'] = self._get_wide(v['name'], self.MAX_TIME)
-            self.results.append(r)
+            self._results.append(r)
 
     @classmethod
     def _get_wide(cls, name, max_time):
@@ -232,8 +255,7 @@ empirical = EmpiricalData()
 
 
 def make_validation_graph(name, y, title):
-
-    fig1 = px.line(empirical.all_states, x='date', y=y, color='state')
+    fig1 = px.line(empirical.states, x='date', y=y, color='state')
     fig1.update_traces(opacity=0.2, showlegend=True)
 
     model_result = modelled.get_result(name)
@@ -265,14 +287,14 @@ total_cases_graph = dcc.Graph(id='total-cases-graph', responsive=True)
 new_cases_graph = dcc.Graph(id='new-cases-graph', responsive=True)
 
 validation_dropdown = html.Div([
-        html.Label('validation'),
-        dcc.Dropdown(
-            id='validation-dropdown',
-            options=[{"label": m, "value": m} for m in modelled.valid_names],
-            value=modelled.valid_names[0],
-            clearable=False
-        ),
-    ]
+    html.Label('validation'),
+    dcc.Dropdown(
+        id='validation-dropdown',
+        options=[{"label": m, "value": m} for m in modelled.valid_names],
+        value=modelled.valid_names[0],
+        clearable=False
+    ),
+]
 )
 
 text = html.Div(
