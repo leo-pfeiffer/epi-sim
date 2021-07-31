806cff318f0ccbff8d6ddc48573731387abe438b
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-31 17:49:46 +0100

    updated test imports

--- app/app/app/tests/test_imports.py
+++ app/app/app/tests/test_imports.py
@@ -5,7 +5,17 @@ from ..log_config import *
 from ..mixins import *
 from ..simulation_files import *
 from ..static_elements import *
-# can't import data_processing or server, as otherwise the downloads
+from ..pages.about import *
+from ..pages.models import *
+from ..pages.networks import *
+from ..pages.not_found import *
+
+# Note: can't import:
+#   - data_processing
+#   - server
+#   - pages.index
+#   - pages.validation
+# since this would trigger the downloads
 
 
 def test_imports():
