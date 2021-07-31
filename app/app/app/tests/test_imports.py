cd7d470016b0882aa63c63f2a98a11dd90b1b5fe
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-07-31 17:48:07 +0100

    test imports

---
+++ app/app/app/tests/test_imports.py
@@ -0,0 +1,13 @@
+# test imports in app
+from ..configuration import *
+from ..layouts import *
+from ..log_config import *
+from ..mixins import *
+from ..simulation_files import *
+from ..static_elements import *
+# can't import data_processing or server, as otherwise the downloads
+
+
+def test_imports():
+    # only need to call this method, don't need to do anything
+    pass
